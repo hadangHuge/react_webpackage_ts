4ce30ed797c62557940f5c6042b91aaa82351510
hadangHuge <445101481@qq.com>
2022-04-05 11:02:47 +0800

    新加schema页面组件

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
+import Schema from './modules/Schema'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Schema />, document.getElementById('root'));
